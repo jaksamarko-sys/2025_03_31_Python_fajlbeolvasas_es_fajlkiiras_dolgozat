ce92eb007a5341cbf3926caa914659b4ec2bed41
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2025-03-31 09:49:46 +0200

    dolgozat_megoldása

--- forma1.py
+++ forma1.py
@@ -8,9 +8,39 @@ Olvasd be az f1.txt adatait, majd oldd meg az alábbi feladatokat!
 
 A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.txt néven!
 """
+forma1 = []
+
+with open ('./beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adatok = sor.strip().split(';')
+        nev = str(adatok[0])
+        team = str(adatok[1])
+        wins = int(adatok[2])
+        runs = int(adatok[3])
+        forma1.append([nev, team, wins, runs])
+
+legtobb_futamgyozelemu_versenyzo = None
+legtobb_futamgyozelem = 0
+for versenyzok in forma1:
+    if legtobb_futamgyozelem < versenyzok[2]:
+        legtobb_futamgyozelem = versenyzok[2]
+        legtobb_futamgyozelemu_versenyzo = versenyzok
+
+legtobb_futam = None
+legtobb_futamos = 0
+for versenyzok in forma1:
+    if legtobb_futamos < versenyzok[3]:
+        legtobb_futamos = versenyzok[3]
+        legtobb_futam = versenyzok
 
 
-print("A beolvasott fájlban összesen ____ versenyző szerepel.")
-print("A legtöbb futamot nyert versenyző: ____")
-print("A legtöbb futamot teljesített versenyző: ____")
-print("Az átlagos futamszám: ____")
+atlag = 0
+for versenyzo in forma1:
+    atlag += versenyzo[3]
+
+with open('./kiirt_adatok/statisztika.txt', 'w', encoding='utf-8') as celfajl:
+    print(f"A beolvasott fájlban összesen {len(forma1)} versenyző szerepel.", file=celfajl)
+    print(f"A legtöbb futamot nyert versenyző: {legtobb_futamgyozelemu_versenyzo[2]}", file=celfajl)
+    print(f"A legtöbb futamot teljesített versenyző: {legtobb_futam}", file=celfajl)
+    print(f"Az átlagos futamszám: {atlag / len(forma1)}", file=celfajl)
